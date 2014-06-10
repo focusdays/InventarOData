07ca30d2903fb637ba6416c207ab6c6af132fdd0
Lolo <lorenz.haenggi@bluewin.ch>
2014-06-11 01:25:05 +0200

    odata-client example

---
+++ src/main/webapp/odata-client/main.js
@@ -0,0 +1,33 @@
+(function() {
+
+  angular.module('app').controller('main',
+  [ 'logger', 'datacontext', controller]);
+  
+  function controller(logger, datacontext) {
+    var vm = this;
+    vm.errorMessage = '';
+    vm.logList = logger.logList;
+    vm.reset = reset;
+    vm.todos = [];
+    
+    getTodos();
+  
+    function getTodos() {
+     datacontext.getAllTodos()
+                .then(success)
+                .catch(failed); 
+            
+      function success(data) {
+          vm.todos = data;
+      }
+      function failed(error) {
+          vm.errorMessage = error.message;
+      }
+    }
+    
+    function reset() {
+      datacontext.reset().then(getTodos);
+    }
+  }
+
+})();
